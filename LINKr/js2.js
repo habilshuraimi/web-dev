a3d5ceb8c0dc53a9dc766a53980027c4ecf81dc6
habilshuraimi <145563681+habilshuraimi@users.noreply.github.com>
2024-08-12 21:21:33 +0530

    Delete LINKr directory

--- LINKr/js2.js
+++
@@ -1,9 +0,0 @@
-let newURL = document.getElementById("shortlink");
-let coptButton = document.getElementById("copy");
-
-coptButton.onclick = ()=>{
-    newURL.select();
-
-    window.navigator.clipboard.writeText(newURL.value);
-}
-// https://amritauniv-my.sharepoint.com/:v:/g/personal/geetham_am_amrita_edu/EesuZ6UAcHhHr3yIIs3MVJsBQA5UoLy0cWWkTeFQb05RsA?nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJTdHJlYW1XZWJBcHAiLCJyZWZlcnJhbFZpZXciOiJTaGFyZURpYWxvZyIsInJlZmVycmFsQXBwUGxhdGZvcm0iOiJXZWIiLCJyZWZlcnJhbE1vZGUiOiJ2aWV3In19&e=2DpWwY
